040b2f21fad1632f7a765cd099a19653dde736ca
Bernardo Esteves <esteveste@users.noreply.github.com>
2018-09-30 17:30:51 +0100

    better table i think

--- Projecto/src/Table.js
+++ Projecto/src/Table.js
@@ -4,7 +4,7 @@
 var legPosArray = [[-26, -9, -8], [-26, -9, 8],
     [26, -9, 8], [26, -9, -8]];
 
-var tableTop_geo = [60, 2, 20];
+var tableTop_geo = [60, 2,21];
 var tableLeg_geo = [2, 2, 16];
 
 
