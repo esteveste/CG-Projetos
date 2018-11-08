c0767ecac8a4db8e56ebaf40b7f1c1995d62709e
André <andrepatricio98@tecnico.ulisboa.pt>
2018-11-08 12:47:36 +0000

    ljjjjbgh

--- Projecto 3/src/Lights.js
+++ Projecto 3/src/Lights.js
@@ -9,23 +9,16 @@ var Lights = function () {
 
 
     global_light = new THREE.DirectionalLight(0xffffff,SUN_INTENSITY);
-    global_light.position.set(1,1,1).normalize();
-    global_light.shadowCameraVisible = true;
-    global_light.shadowCameraNear = 1;
-    global_light.shadowCameraFar = 150;
-    global_light.castshadow = true;
-    global_light.castShadow=true;
-    // global_light.position.set( 0, 1, 1 ).normalize();
+    global_light.position.set(1,1,1);
     this.add(global_light);
 
     for (let i=0; i<4; i++){
-        slight = new THREE.SpotLight( 0xffffff,1,90,Math.PI/5,10 );
+        slight = new THREE.SpotLight( 0xffffff,2,90,Math.PI/5,10 );
         slight.position.set(...slight_positions[i]);
         slight.target.position.set(0,0,0);
         slight.castShadow = true;
         slight.penumbra=.2;
         this.add(slight);
-        this.add(slight.target);
         //let spotter = new THREE.SpotLightHelper(slight);
         //this.add(spotter);
     }
@@ -43,8 +36,8 @@ Lights.prototype.changeSun=function(){
 };
 
 Lights.prototype.changeLightOnOff=function(lightN){
-    if(this.children[lightN*2 + 1].intensity == 0)
-        this.children[lightN*2 + 1].intensity = 1;
+    if(this.children[lightN + 1].intensity == 0)
+        this.children[lightN + 1].intensity = 1;
     else
-        this.children[lightN*2 + 1].intensity = 0;
+        this.children[lightN + 1].intensity = 0;
 };
